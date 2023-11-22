bd07f7ad02b74c2789afcb92696ba4fe3acf1008
@AbdulAziz471 <aziztariq2211@gmail.com>
2023-11-22 19:18:39 +0500

    added

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,3 +1,12 @@
+import { useState } from "react";
+
 export default function Default(props) {
-  return <></>;
+  const [count, setCount] = useState(0); // Initializing state
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
 }
